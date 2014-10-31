8d9c20c12db93622981de53e96f8ed0951595719
Eleanor Paxman <e.paxman@business.uq.edu.au>
2014-10-31 11:23:34 +1000

    Some styling

--- upload.php
+++ upload.php
@@ -8,69 +8,78 @@
 
 <body>
 
-<form action="upload-image.php" method="POST" enctype="multipart/form-data">
+<header>
+	<div class="inner">Curagrapher</div>
+</header>
 
-<label>Upload image</label><input type="file" name="file" id="file" />
+<form action="upload-image.php" method="POST" enctype="multipart/form-data">
 
 <!-- thumbnail preview -->
 
-<label>Category</label>
-<!-- select multiple -->
-<select name="category">
-	<option>Landscape</option>
-	<option>Buildings</option>
-	<option>People</option>
-</select>
+<fieldset>
+	<legend>1. Locate</legend>
 
-<label>Location</label>
-<!-- list of world cities? use exif data? -->
-<select name="location">
-	<optgroup label="Brisbane">
-		<option>Toowong</option>
-		<option>South Bank</option>
-	</optgroup>
-	<optgroup label="Sunshine Coast">
-		<option>Peregian</option>
-		<option>Caloundra</option>
-		<option>Mapleton</option>
-	</optgroup>
-	<option>Canberra</option>
-	<option>Perth</option>
-	<option>Melbourne</option>
-	<optgroup label="New Zealand">
-		<option>Queenstown</option>
-		<option>Blenheim</option>
-	</optgroup>
-</select>
+	<!--<label>Category</label>
+	<select name="category">
+		<option>Landscape</option>
+		<option>Buildings</option>
+		<option>People</option>
+	</select>-->
 
-<label>Time of day</label>
-<!-- auto? -->
-<select name="timeofday">
-	<option>Dawn</option>
-	<option>Morning</option>
-	<option>Afternoon</option>
-	<option>Dusk</option>
-	<option>Evening</option>
-</select>
+	<label>location</label>
+	<!-- list of world cities? use exif data? -->
+	<select name="location">
+		<optgroup label="Brisbane">
+			<option>Toowong</option>
+			<option>South Bank</option>
+		</optgroup>
+		<optgroup label="Sunshine Coast">
+			<option>Peregian</option>
+			<option>Caloundra</option>
+			<option>Mapleton</option>
+		</optgroup>
+		<option>Canberra</option>
+		<option>Perth</option>
+		<option>Melbourne</option>
+		<optgroup label="New Zealand">
+			<option>Queenstown</option>
+			<option>Blenheim</option>
+		</optgroup>
+	</select>
 
-<label>Time of year</label>
-<!-- auto? -->
-<select name="timeofyear">
-	<option>Summer</option>
-	<option>Autumn</option>
-	<option>Winter</option>
-	<option>Spring</option>
-</select>
+	<label>time of day</label>
+	<!-- auto? -->
+	<select name="timeofday">
+		<option>Dawn</option>
+		<option>Morning</option>
+		<option>Afternoon</option>
+		<option>Dusk</option>
+		<option>Evening</option>
+	</select>
 
-<label>Tags: content</label><input type="text" />
+	<label>time of year</label>
+	<!-- auto? -->
+	<select name="timeofyear">
+		<option>Summer</option>
+		<option>Autumn</option>
+		<option>Winter</option>
+		<option>Spring</option>
+	</select>
+</fieldset>
 
-<label>Tags: people</label><input type="text" />
+<fieldset class="tags">
+	<legend>2. Describe</legend>
+	<label>content</label><input type="text" />
+	<label>people</label><input type="text" />
+	<label>composition</label><input type="text" />
+	<label>other</label><input name="tag" type="text" />
+</fieldset>
 
-<label>Tags: composition</label><input type="text" />
-
-<label>Tags: other</label><input name="tag" type="text" />
-
-<input type="submit" name="submit" value="Add photo" />
+<fieldset>
+	<legend>3. Upload</legend>
+	<input type="file" name="file" id="file" value="choose file" />
+	<input type="submit" name="submit" value="Add image" />
+</fieldset>
 
 <form>
 
